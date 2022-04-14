f8be9acf17cb2d31c1f1a99a344203bca85bab45
Edward <73746306+WangEdward@users.noreply.github.com>
2022-04-14 09:33:36 +0800

    微调与修复
    run.py 加入系统检测，使得可在 Windows 和 Unix-based 系统上运行
    将 up_location 改成了 Array 类型，可以同时上传多个位置
    api.py 修改成了if 判断，match case 似乎已经无法在较新版本的 python 中使用了
    乱乱地修改了格式，使代码看起来更加凌乱啦（雾

--- api.py
+++ api.py
@@ -1,79 +1,82 @@
-import requests,json,config
+import config
+import json
+import requests
 from bs4 import BeautifulSoup
 
-def upload(location,imgpath,name):
-    match location:
-        #bilibili图床-20MB
-        case 0:
+
+def upload(location, imgpath, name):
+    try:
+        # bilibili图床-20MB
+        if location == 0:
             url = "https://api.vc.bilibili.com/api/v1/drawImage/upload"
             head = {
                 "User-Agent": "bilibili",
-                "Cookie":config.bilibili_cookie
+                "Cookie": config.bilibili_cookie
             }
-            file ={
+            file = {
                 "file_up": (name, open(imgpath, "rb"), "image/png"),
-                "biz": (None,"text"),
-                "category": (None,"daily")
+                "biz": (None, "text"),
+                "category": (None, "daily")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()["data"]["image_url"]
+            zz = requests.post(url=url, files=file, headers=head).json()["data"]["image_url"]
             return zz
-        #网易严选图床-10MB
-        case 1:
+        # 网易严选图床-10MB
+        if location == 1:
             url = "http://you.163.com/xhr/file/upload.json"
             head = {
                 "user-agent": "",
-                "cookie":config.wyyx_cookie
+                "cookie": config.wyyx_cookie
             }
-            file ={
+            file = {
                 "file": (name, open(imgpath, "rb"), "image/png")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()
+            zz = requests.post(url=url, files=file, headers=head).json()
             return zz["data"][0]
-        #搜狗图床-10MB
-        case 2:
+        # 搜狗图床-10MB
+        if location == 2:
             url = "https://api.kinh.cc/Picture/SoGou.php"
             head = {
-                "user-agent":config.ua
+                "user-agent": config.ua
             }
-            file ={
+            file = {
                 "FilePicture": (name, open(imgpath, "rb"), "image/png")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()
+            zz = requests.post(url=url, files=file, headers=head).json()
             return zz['link']
-        #美团图床-10MB
-        case 3:
+        # 美团图床-10MB
+        if location == 3:
             url = "https://api.kinh.cc/Picture/MeiTuan.php"
             head = {
-                "user-agent":config.ua
+                "user-agent": config.ua
             }
-            file ={
+            file = {
                 "FilePicture": (name, open(imgpath, "rb"), "image/png")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()
+            zz = requests.post(url=url, files=file, headers=head).json()
             return zz['link']
-        #今日头条-10MB
-        case 4:
+        # 今日头条-10MB
+        if location == 4:
             url = "https://mp.toutiao.com/mp/agw/article_material/photo/upload_picture?type=ueditor&pgc_watermark=0&action=uploadimage&encode=utf-8&is_private=1"
             head = {
-                "user-agent":config.ua,
-                "cookie":config.jrtt_cookie
+                "user-agent": config.ua,
+                "cookie": config.jrtt_cookie
             }
-            file ={
+            file = {
                 "upfile": (name, open(imgpath, "rb"), "image/png")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()
+            zz = requests.post(url=url, files=file, headers=head).json()
             return zz['url']
-        #一加论坛-10MB
-        case 5:
+        # 一加论坛-10MB
+        if location == 5:
             url = "https://www.oneplusbbs.com/misc.php?mod=swfupload&operation=uploadimg&simple=1&type=image"
             head = {
-                "user-agent":config.ua,
-                "cookie":config.qdyd_cookie
+                "user-agent": config.ua,
+                "cookie": config.qdyd_cookie
             }
-            file ={
+            file = {
                 "Filedata": (name, open(imgpath, "rb"), "image/png"),
-                "uid": (None,"3437606"),
-                "hash": (None,"5e9cafdd8723d52408218f2bde4339d6")
+                "uid": (None, "3437606"),
+                "hash": (None, "5e9cafdd8723d52408218f2bde4339d6")
             }
             data = {
                 "mod": "swfupload",
@@ -81,60 +84,62 @@ def upload(location,imgpath,name):
                 "simple": 1,
                 "type": "image"
             }
-            zz = requests.post(url=url,data=data,files=file,headers=head).json()
+            zz = requests.post(url=url, data=data, files=file, headers=head).json()
             if zz['code'] == 200:
                 return zz['link']
             else:
                 return zz['msg']
-        #qq图床-5MB
-        case 6:
+        # qq图床-5MB
+        if location == 6:
             url = "https://pic.ihcloud.net/api2/qq.php"
             head = {
                 "referer": "https://pic.ihcloud.net",
-                "user-agent":config.ua
+                "user-agent": config.ua
             }
-            file ={
+            file = {
                 "image": (name, open(imgpath, "rb"), "image/png"),
-                "file_id": (None,"0")
+                "file_id": (None, "0")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()
+            zz = requests.post(url=url, files=file, headers=head).json()
             return zz["data"]["url"]
-        #遇见图床-葫芦侠接口-5MB
-        case 7:
+        # 遇见图床-葫芦侠接口-5MB
+        if location == 7:
             url = "https://www.hualigs.cn/api/upload"
             head = {
-                "user-agent":config.ua
+                "user-agent": config.ua
             }
-            file ={
+            file = {
                 "image": (name, open(imgpath, "rb"), "image/png"),
-                "token": (None,"f55a7e46fa8c5614108ff3fbc4986c86"),
-                "apiType": (None,"huluxia")
+                "token": (None, "f55a7e46fa8c5614108ff3fbc4986c86"),
+                "apiType": (None, "huluxia")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()
+            zz = requests.post(url=url, files=file, headers=head).json()
             return zz['data']['url']['huluxia']
-        #cnmo论坛-5MB
-        case 8:
+        # cnmo论坛-5MB
+        if location == 8:
             url = "https://bbs.cnmo.com/index.php?c=Api_Attachment&m=UploadImageNew"
             head = {
-                "user-agent":config.ua
+                "user-agent": config.ua
             }
-            file ={
+            file = {
                 "img": (name, open(imgpath, "rb"), "image/png"),
-                "uid": (None,"12273065")
+                "uid": (None, "12273065")
             }
-            zz = requests.post(url=url,files=file,headers=head).json()
+            zz = requests.post(url=url, files=file, headers=head).json()
             return zz['data']['url']
-        #起点阅读图床-2MB
-        case 9:
+        # 起点阅读图床-2MB
+        if location == 9:
             url = "https://my.qidian.com/ajax/headimage/uploadimg"
             head = {
-                "user-agent":config.ua,
-                "cookie":config.qdyd_cookie
+                "user-agent": config.ua,
+                "cookie": config.qdyd_cookie
             }
-            file ={
+            file = {
                 "image": (name, open(imgpath, "rb"), "image/png")
             }
-            zz = requests.post(url=url,files=file,headers=head).text
-            soup = BeautifulSoup(zz,'html.parser')
+            zz = requests.post(url=url, files=file, headers=head).text
+            soup = BeautifulSoup(zz, 'html.parser')
             data = json.loads(soup.body.string)
-            return "https:"+data["data"]["url"]
+            return "https:" + data["data"]["url"]
+    except Exception as e:
+        return e
--- run.py
+++ run.py
@@ -1,37 +1,49 @@
-import os,api,random,config
+import api
+import config
+import os
+import random
+import platform
 
 
-#获取图片大小
+# 获取图片大小
 def size(filePath):
-    with open(filePath,"rb") as f:
+    with open(filePath, "rb") as f:
         size = len(f.read())
     return size / 1e6
 
+
 def locations(imgsize):
     if imgsize < 2:
-        return random.randint(0,8)
+        return random.randint(0, 8)
     elif imgsize < 5:
-        return random.randint(0,7)
+        return random.randint(0, 7)
     elif imgsize < 10:
-        return random.randint(0,4)
+        return random.randint(0, 4)
     else:
         return 0
 
+
 def run():
-    aa = ["bilibili","网易严选","搜狗","美团","今日头条","一加论坛","qq","葫芦侠","cnmo论坛","起点阅读"]
-    filepath = os.getcwd()+"\\pic"
-    for i,j,k in os.walk(filepath):
+    aa = ["bilibili", "网易严选", "搜狗", "美团", "今日头条", "一加论坛", "qq", "葫芦侠", "cnmo论坛", "起点阅读"]
+    # 获取当前系统
+    if platform.system() == "Windows":
+        sep = "\\"
+    else:
+        sep = "/"
+    filepath = os.getcwd() + sep + "pic"
+    for i, j, k in os.walk(filepath):
         for j in k:
-            #获取图片路径
-            imgpath = filepath + "\\" + j
-            #获取图片名字
+            # 获取图片路径
+            imgpath = filepath + sep + j
+            # 获取图片名字
             name = j[:-4]
             imgsize = size(imgpath)
             location = locations(imgsize)
             img_location = aa[location]
-            if config.up_location == None:
-                data = api.upload(location,imgpath,j)
-                print(f"{name}-上传成功:{data}--上传位置:{img_location}")
-            else:
-                data = api.upload(config.up_location,imgpath,j)
-                print(f"{name}-上传成功:{data}--上传位置:{aa[config.up_location]}")
+            for i in config.up_location:
+                if i == None:
+                    data = api.upload(location, imgpath, j)
+                    print(f"{name}-上传成功:{data} --上传位置:{img_location}")
+                else:
+                    data = api.upload(i, imgpath, j)
+                    print(f"{name}-上传成功:{data} --上传位置:{aa[i]}")
